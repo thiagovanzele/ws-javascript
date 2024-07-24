8a68c0e41582061da66017b9b361182310cae9a4
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-24 18:10:04 -0300

    Ex001 e Ex002

---
+++ ex001/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite o primeiro número');
+let num2 = prompt('Digite o segundo número');
+
+const resultado = parseInt(num1) + parseInt(num2);
+
+alert(`${num1} + ${num2} = ${resultado}`);
