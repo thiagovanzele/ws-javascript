75893ba93e65a34f03b379b85ad0ee24d1ebe590
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-27 12:22:30 -0300

    Ex010

---
+++ ex010/main.js
@@ -0,0 +1,10 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura 
+// de uma Imagem. Retorne true se a imagem estiver no modo paisagem.
+
+// function ePaisagem(largura, altura) {
+//     return largura > altura;
+// }
+
+
+const paisagem = (largura, altura) => largura > altura;
+console.log(paisagem(100, 50));
